93fd16ac42c059b0ce48a43969cb1952f38dda8a
Caleb <c.ramdath.26@gmail.com>
2024-11-16 23:35:13 -0400

    Added Testing Sample Files

---
+++ demo/src/test/java/com/example/TestExtractor.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestExtractor {
+    
+    @Test
+    public void testValidSolution() throws IOException {
+
+
+    }
+}
---
+++ demo/target/test-classes/com/example/README.txt
@@ -0,0 +1,6 @@
+Test files used Jahiem_Ceaser_816045438_A1 as a base
+Jahiem_Ceaser_816045438_A1 should be a properly working file
+Nicholas_Mendez_816012345_A1 is a folder which lacks any content
+Caleb_Ramdath_816038072_A1 contains empty Java Files
+Jane_Doe_8160312545_A1 contains only 2 out of the 4 classes
+John_Doe_816032145_A1 contains the 4 files with a compliatoin error
